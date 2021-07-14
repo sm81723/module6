7068b9706d197a0917fadebcf7b85075cf87cd5a
Sean Mattingly <seanmattingly@icloud.com>
2021-07-14 19:50:41 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='module6',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Sean Mattingly',
+    author_email='scm0092@auburn.edu',
+    description='Underwater_game'
+)
